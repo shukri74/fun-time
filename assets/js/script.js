4f35f7ea91e6f597012be5adb3de3c073b45a20d
Haroon <mharoonbutt13@gmail.com>
2023-02-04 19:40:37 +0000

    show more games api functionality configured

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,55 @@
-$(".search").on("click", function(event) {
+$( document ).ready(function() {
+  var apiKey = "f41a02823b9a4150a843324d295e2ae9";
+  // var queryURL = "https://www.freetogame.com/api/games";
+  var queryURL = "https://api.rawg.io/api/games?key="+ apiKey +"&page_size=3";
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var res = response.results;
+    console.log(res)
+    for (let i = 0; i < res.length; i++) {
+      console.log(res[i].name);
+      // var imgURL = res[i].background_image;
+
+      var cardDiv = $("<div>").attr("class","card");
+      var cardImage = $("<img>").attr({class:"card-img-top",src:res[i].background_image});
+      cardDiv.append(cardImage);
+      var cardBody = $("<div>").attr("class","card-body");
+      var cardTitle = $("<h5>").attr("class","card-title").text(res[i].name);
+      cardBody.append(cardTitle);
+      // var gHeading = $('<strong>').text('Genre: ');
+      var genreP = $('<p>').text(res[i].genres[0].name);
+      // genreP.text(res[i].genres[0].name);
+      cardBody.append(genreP);
+      var plateform = $('<p>').text(res[i].rating);
+      cardBody.append(plateform);
+      cardDiv.append(cardBody);
+      var cardFooter = $("<div>").attr("class","card-footer");
+      var releasedText = $("<small>").attr("class","text-muted").text(res[i].released);
+      cardFooter.append(releasedText)
+      cardDiv.append(cardFooter);
+      $('#more-game-section').append(cardDiv);
+    }
+  });
+
+
+
+
+
+
+
+  $(".search").on("click", function(event) {
     event.preventDefault();
   
     // Here we grab the text from the input box
     var search = $("#search").val();
-    var apiKey = "f41a02823b9a4150a843324d295e2ae9";
     console.log(search);
   
     // Here we construct our URL
-    var queryURL = "https://api.rawg.io/api/games?key="+ apiKey +"&page_size=3&search=" + search;
+    queryURL='';
+    queryURL = "https://api.rawg.io/api/games?key="+ apiKey +"&page_size=3&search=" + search;
   
     $.ajax({
       url: queryURL,
@@ -39,3 +81,9 @@ $(".search").on("click", function(event) {
   
   
   });
+
+});
+
+
+
+
